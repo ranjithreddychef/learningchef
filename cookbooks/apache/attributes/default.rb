472498c00eae0ce20a763ba5bc91f47afb6dcfc2
root <root@ranjith-reddy1.mylabserver.com>
2017-02-21 21:35:21 +0000

    my chef repo on git for first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["ranjith-reddy3"] = { "site_title" => "ranjith-reddy3s websites coming soon", "port" => 80, "domain" => "ranjith-reddy3.mylabserver.com" }
+default["apache"]["sites"]["ranjith-reddy3b"] = { "site_title" => "ranjith-reddy3b websites coming soon", "port" => 80, "domain" => "ranjith-reddy3b.mylabserver.com" }
+
